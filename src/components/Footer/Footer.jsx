262a56c97dc34631ec2971fbe32bd1e0e1b6c723
SAIEED12 <abusaieed0@gmail.com>
2026-04-02 23:58:26 +0600

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p><small>Thank you for visiting!</small></p>
+            <a href="/">Privacy</a>
+            <a href="/fb">Social</a>
+        </div>
+    );
+};
+
+export default Footer;
